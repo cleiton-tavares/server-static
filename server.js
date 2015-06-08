145889b1e8d2b347c50bde1ad8cbad49b51e2a6a
Cleiton Tavares <cleitontavares@live.com>
2015-06-08 10:46:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(1221);
+
+
+console.log('API rodando em  http://%s:%s : 1221');
